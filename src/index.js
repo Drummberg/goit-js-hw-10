2708102430277d15d9711b4e0521bdbadaddc797
Drummberg <drummberg@gmail.com>
2022-02-07 15:15:51 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import Notiflix from 'notiflix'
+import 'notiflix/dist/notiflix-3.2.4.min.css'
+import debounce from 'lodash.debounce'
+
+
+
 
 const DEBOUNCE_DELAY = 300;
