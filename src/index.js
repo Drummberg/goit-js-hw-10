3b32c1aeac6f9452bda4af22d034ea460ac2a707
Drummberg <drummberg@gmail.com>
2022-02-09 01:45:55 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -10,28 +10,58 @@ const searchInput = document.querySelector('#search-box')
 const countryList = document.querySelector('.country-list')
 const countryInfo = document.querySelector('.country-info')
 
-searchInput.addEventListener('input', onSearch);
+searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
     event.preventDefault();
 
-   const searchQuery = searchInput.value.trim();
+   const name = searchInput.value.trim();
 
-    if (searchQuery === '') {
-        return (countryList = ''), (countryInfo = '')
-    }
+    if (name === '') {
+        return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+        }
     
     fetchCountries(name)
-        
-
-    
-
+        .then(countries => {
+            countryList.innerHTML = ''
+            countryInfo.innerHTML = ''
+            if (countries.length === 1) {
+                countryList.insertAdjacentHTML('beforeend', renderList(countries))
+                countryInfo.insertAdjacentHTML('beforeend', renderInfo(countries))
+            } else if (countries.length >= 10) {
+                alertVeryBigChoice();
+            } else {
+                countryList.insertAdjacentHTML('beforeend', renderList(countries))
+            }
+        })
+       .catch(onFetchError) 
 }
 
-function onFetchError(error) {
-    Notiflix.Notify.failure('Oops, there is no country with that name');
+function renderList(countries) {
+    const markup = countries.map(({ name, flags }) =>{
+        return `<li class="country-list__item">
+        <img class="country-list__flag" src="${flags.svg}" alt="Flag ${name.official}" width=30px heigth=30px>
+        <h2 class="country-list_name">${name.official}</h2>
+        </li> `
+    }).join('')
+    return markup
+}
+
+function renderInfo(countries) {
+    const markup = countries.map(({ capital, population, languages }) => {
+        return `<ul class="country-info__list"
+                    <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+                    <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+                    <li class="country-info__item"><p><b>Languages: </b>${languages}</p></li>
+                </ul>`
+    }).join('')
+    return markup
+}
+
+function onFetchError() {
+    Notiflix.Notify.failure('Oops, there is no country with that name')
 }
 
 function alertVeryBigChoice() {
-  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+  Notiflix.Notify.info('Too many matches found. Please enter a more specific name')
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,10 +3,12 @@ const fields = 'fields=name,capital,population,flags,languages'
 
 
  export function fetchCountries(name) {
-    return fetch(`${BASE_URL}${name}&${fields}`)
+    return fetch(`${BASE_URL}${name}?${fields}`)
         .then(response => response.json())
         .catch(error => console.log(error))
        
 }
+
+ 
 
 
