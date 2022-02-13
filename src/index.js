2a376109d56b9ca88cd1a47db7be74352212d8b5
Drummberg <drummberg@gmail.com>
2022-02-13 19:06:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function renderInfo(countries) {
         return `<ul class="country-info__list"
                     <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
                     <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
-                    <li class="country-info__item"><p><b>Languages: </b>${languages.value}</p></li>
+                    <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
                 </ul>`
     }).join('')
     return markup
